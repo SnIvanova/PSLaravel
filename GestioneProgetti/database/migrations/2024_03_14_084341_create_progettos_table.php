148ffbd4b393b0da17d40b1a5e1cc91dfc6f659e
SnIvanova <1snizhanaivanova@gmail.com>
2024-03-14 12:18:43 +0100

    tables

--- GestioneProgetti/database/migrations/2024_03_14_084341_create_progettos_table.php
+++ GestioneProgetti/database/migrations/2024_03_14_084341_create_progettos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('progettos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
             $table->timestamps();
         });
     }
--- GestioneProgetti/database/migrations/2024_03_14_084413_create_attivitas_table.php
+++ GestioneProgetti/database/migrations/2024_03_14_084413_create_attivitas_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('attivitas', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('progettos')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->on('utentes')->references('id')
+                    ->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
