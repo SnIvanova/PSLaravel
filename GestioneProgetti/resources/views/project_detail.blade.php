b4fd8edc46173180b901675dbaef05eaaa6b3993
SnIvanova <1snizhanaivanova@gmail.com>
2024-03-19 08:58:16 +0100

    main

--- GestioneProgetti/resources/views/project_detail.blade.php
+++ GestioneProgetti/resources/views/project_detail.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-8 px-4 sm:px-6 lg:px-8">
+    <div class="my-3 py-8 px-4 sm:px-6 lg:px-8">
         <div class="max-w-3xl mx-auto">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div class="bg-white border rounded-md p-6">
